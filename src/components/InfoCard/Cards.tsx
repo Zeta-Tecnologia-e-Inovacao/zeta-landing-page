4b38c9d4e8dd8e1b508d4038b0161f30c68086f3
Pedro Henrique Mota <pedrohmota2005@gmail.com>
2023-04-11 21:47:22 -0300

    refactor: change folder names

--- src/components/InfoCard/Cards.tsx
+++ src/components/InfoCard/Cards.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Navbar, Grid, Container, Text } from "@nextui-org/react";
 
+
 import InfoCard from "./infoCard";
 
 
@@ -11,30 +12,27 @@ export default function App() {
         <Grid.Container gap={2}>
             <Grid xs={12} sm={4}>
                 <InfoCard
-                label="Course"
                 title="Iot"
                 imageURL="https://littlevisuals.co/images/red_dawn.jpg"
-                studentCount="3,500"/>
+                />
             </Grid>
 
             <Grid xs={12} sm={4}>
                 <InfoCard
-                label="Course"
                 title="Sistemas"
                 imageURL="https://littlevisuals.co/images/sunset.jpg"
-                studentCount="4,200"/>
+                />
             </Grid>
 
             <Grid xs={12} sm={4}>
                 <InfoCard
-                label="Course"
                 title="Compressor Inteligente"
                 imageURL="https://littlevisuals.co/images/tail.jpg"
-                studentCount="1,000"/>
+                />
             </Grid>
 
         </Grid.Container>
    </Container>     
   
-  )
-}
+  );
+};
--- src/components/InfoCard/infoCard.tsx
+++ src/components/InfoCard/infoCard.tsx
@@ -3,26 +3,14 @@ import type {NextPage} from 'next';
 
 interface Props{
   title: string;
-  label: string;
   imageURL: string;
-  studentCount: string;
 }
 
 const InfoCard: NextPage<Props> = (props) => {
 
-  const { title, label, imageURL, studentCount } = props;
+  const { title, imageURL } = props;
   return(
     <Card>
-        <Card.Header css={{position: "absolute", top: 5}}>
-          <Col>
-            <Text size={12} weight="bold" transform="uppercase" color="#ffffffAA">
-              {label}
-            </Text>
-            <Text h4 color="white">
-              {title}
-            </Text>
-          </Col>
-          </Card.Header>
         <Card.Image src={imageURL}></Card.Image>
         <Card.Footer isBlurred
         css={{
@@ -34,7 +22,7 @@ const InfoCard: NextPage<Props> = (props) => {
         <Row>
           <Col>
             <Text color="#d1d1d1" size={18}>
-              {studentCount} serviços
+              {title}
             </Text>
           </Col>
           <Col>
@@ -58,62 +46,7 @@ const InfoCard: NextPage<Props> = (props) => {
         </Card.Footer>
        
     </Card>
-  )
-}
-
-// export default function CardPage() {
-//   const list = [
-//     {
-//       title: "Iot",
-//       img: "/images/fruit-1.jpeg",
-//       price: "$5.50",
-//     },
-//     {
-//       title: "Compressor Inteligente",
-//       img: "/images/fruit-2.jpeg",
-//       price: "$3.00",
-//     },
-//     {
-//       title: "Cherry",
-//       img: "/images/fruit-3.jpeg",
-//       price: "$10.00",
-//     },
-//     {
-//       title: "Lemon",
-//       img: "/images/fruit-4.jpeg",
-//       price: "$5.30",
-//     },
-    
-   
-//   ];
-
-//   return (
-//     <Grid.Container gap={1} justify="flex-start">
-//       {list.map((item, index) => (
-//         <Grid xs={4} sm={3} key={index}>
-//           <Card isPressable>
-//             <Card.Body css={{ p: 0 }}>
-//               <Card.Image
-//                 src={"https://nextui.org" + item.img}
-//                 objectFit="cover"
-//                 width="100%"
-//                 height={200}
-//                 alt={item.title}
-//               />
-//             </Card.Body>
-//             <Card.Footer css={{ justifyItems: "flex-start" }}>
-//               <Row wrap="wrap" justify="space-between" align="center">
-//                 <Text b>{item.title}</Text>
-//                 <Text css={{ color: "$accents7", fontWeight: "$semibold", fontSize: "$sm" }}>
-//                   {item.price}
-//                 </Text>
-//               </Row>
-//             </Card.Footer>
-//           </Card>
-//         </Grid>
-//       ))}
-//     </Grid.Container>
-//   );
-// }
+  );
+};
 
 export default InfoCard;
