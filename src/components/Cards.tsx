381728c35e57ce9764646858f95887380fe140e1
Pedro Henrique Mota <pedrohmota2005@gmail.com>
2023-04-12 17:46:06 -0300

    style: new indentation

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -1,36 +1,45 @@
-import React from "react";
-import { Grid, Container } from "@nextui-org/react";
-import InfoCard from "./infoCard";
+import React from 'react';
+import { Grid, Container } from '@nextui-org/react';
+import type { NextPage } from 'next';
+import InfoCard from './infoCard';
 
+const CardPage: NextPage = () => (
 
-export default function App() {
- 
-  return (
-   <Container>
-        <Grid.Container gap={2}>
-            <Grid xs={12} sm={4}>
-                <InfoCard
-                title="Iot"
-                imageURL="https://littlevisuals.co/images/red_dawn.jpg"
-                />
-            </Grid>
+  <Container>
+    <Grid.Container
+      gap={2}
+    >
+      <Grid
+        xs={12}
+        sm={4}
+      >
+        <InfoCard
+          title='Iot'
+          imageURL='https://littlevisuals.co/images/red_dawn.jpg'
+        />
+      </Grid>
 
-            <Grid xs={12} sm={4}>
-                <InfoCard
-                title="Sistemas"
-                imageURL="https://littlevisuals.co/images/sunset.jpg"
-                />
-            </Grid>
+      <Grid
+        xs={12}
+        sm={4}
+      >
+        <InfoCard
+          title='Sistemas'
+          imageURL='https://littlevisuals.co/images/sunset.jpg'
+        />
+      </Grid>
 
-            <Grid xs={12} sm={4}>
-                <InfoCard
-                title="Compressor Inteligente"
-                imageURL="https://littlevisuals.co/images/tail.jpg"
-                />
-            </Grid>
+      <Grid
+        xs={12}
+        sm={4}
+      >
+        <InfoCard
+          title='Compressor Inteligente'
+          imageURL='https://littlevisuals.co/images/tail.jpg'
+        />
+      </Grid>
+    </Grid.Container>
+  </Container>
+);
 
-        </Grid.Container>
-   </Container>     
-  
-  );
-};
+export default CardPage;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,44 +1,28 @@
-import React from "react";
-import { Navbar, Link, Text, Avatar, Dropdown } from "@nextui-org/react";
-import { ZetaLogo } from "./ZetaLogo.jsx";
+import React from 'react';
+import { Navbar, Text } from '@nextui-org/react';
+import type { NextPage } from 'next';
 
-export default function App() {
+const Header: NextPage = () => (
+  <Navbar className='text-transition-slider-navbar' isBordered variant='sticky'>
+    <Navbar.Toggle showIn='xs' />
+    <Navbar.Brand
+      css={{
+        '@xs': {
+          w: '12%',
+        },
+      }}
+    >
+      <Text b color='inherit' hideIn='xs' size={25}>
+        Zeta
+      </Text>
+    </Navbar.Brand>
+    <Navbar.Content>
+      <Navbar.Link isActive href='#'>Home</Navbar.Link>
+      <Navbar.Link href='#'>Customers</Navbar.Link>
+      <Navbar.Link href='#'>Soluções</Navbar.Link>
+      <Navbar.Link href='#'>Sobre nós</Navbar.Link>
+    </Navbar.Content>
+  </Navbar>
+);
 
-  return (
-  
-      <Navbar className='text-transition-slider-navbar' isBordered variant="sticky" >
-        <Navbar.Toggle showIn="xs" />
-        <Navbar.Brand
-          css={{
-            "@xs": {
-              w: "12%",
-            },
-          }}
-        >
-          <ZetaLogo />
-          <Text b color="inherit" hideIn="xs" size={25}>
-            Zeta
-          </Text>
-        </Navbar.Brand>
-        <Navbar.Content>
-          <Navbar.Link isActive href="#">Home</Navbar.Link>
-          <Navbar.Link href="#">
-            Customers
-          </Navbar.Link>
-          <Navbar.Link href="#">Soluções</Navbar.Link>
-          <Navbar.Link href="#">Sobre nós</Navbar.Link>
-        </Navbar.Content>
-        <Navbar.Content
-          css={{
-            "@xs": {
-              w: "12%",
-              jc: "flex-end",
-            },
-          }}
-        >
-        </Navbar.Content>
-      </Navbar>
-  
-  );
-};
-
+export default Header;
