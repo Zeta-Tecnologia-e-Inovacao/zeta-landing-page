9f63fdbf60aa598840888bf079ddbb17a481ab48
Pedro Henrique Mota <pedrohmota2005@gmail.com>
2023-04-14 18:44:07 -0300

    feat: new routes

---
+++ src/pages/missao-valores/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import type { NextPage } from 'next';
+
+const MissaoValores: NextPage = () => (<h1>Olá Missao Valores!</h1>);
+
+export default MissaoValores;
---
+++ src/pages/quem-somos/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import type { NextPage } from 'next';
+
+const Header: NextPage = () => (<h1>Olá mundo!</h1>);
+
+export default Header;
