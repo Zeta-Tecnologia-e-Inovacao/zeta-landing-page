08d49c96233489b1ffd72c7f5a5fb6309f25441a
Pedro Henrique Mota <pedrohmota2005@gmail.com>
2023-04-17 20:16:35 -0300

    fix: PR Jumbotron

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,9 +7,6 @@ const darkTheme = createTheme({ type: 'dark' });
 
 const App = ({ Component, pageProps }: AppProps) => (
   <NextUIProvider theme={darkTheme}>
-    {/* <Script
-      src='https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js'>
-    </Script> */}
     <Component {...pageProps} />
     <GlobalStyle />
   </NextUIProvider>
