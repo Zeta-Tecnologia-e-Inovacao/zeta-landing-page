a4b9c39cbe144047a8925125fc3283ec6a0e84f8
Pedro Henrique Mota <pedrohmota2005@gmail.com>
2023-04-18 20:23:08 -0300

    refactor: add Script (bootstrap)

--- src/components/jumbotron/teste.tsx
+++ src/components/jumbotron/teste.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import type { NextPage } from 'next';
 import { Tooltip, Button } from '@nextui-org/react';
+import Script from 'next/script';
 import * as S from './styles';
 
 const JumbotronTeste: NextPage = () => (
 
   <S.JumbotronConfig>
     <div className='container mt-3'>
-      <div className='mt-4 p-5 ground text-white rounded'>
+      <Script src='https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js' />
+      <div className='mt-4 p-5 ground text-black rounded'>
         <h1>Impulsione o potencial do seu negócio com nossas soluções TI de última geração.</h1>
         <p>
           Nós fornecemos soluções personalizadas que ajudam sua startup a crescer
