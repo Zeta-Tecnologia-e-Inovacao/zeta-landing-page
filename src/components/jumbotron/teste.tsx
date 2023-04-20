cf25fd1f4f856115b81b0fafcab54940e460cbe3
Pedro Henrique Mota <pedrohmota2005@gmail.com>
2023-04-20 12:05:38 -0300

    refactor: new tests on the jumbotron

--- src/components/jumbotron/teste.tsx
+++ src/components/jumbotron/teste.tsx
@@ -7,14 +7,18 @@ const JumbotronTeste: NextPage = () => (
 
   <S.JumbotronConfig>
     {/* <div className='container'> */}
-    <Script src='https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js' />
-    <div className='p-5 jumbo text-white'>
-      <h2 className='text-jumbo'>Impulsione o potencial do seu </h2>
-      <h2>negócio com nossas soluções TI</h2>
-      <h2>de última geração.</h2>
-      <h3>Nós fornecemos soluções personalizadas</h3>
-      <h3>que ajudam sua startup a crescer</h3>
-      <h3>e se destacar no mercado digital.</h3>
+    <div className='container slide-top'>
+      <Script src='https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js' />
+      <div className='p-5 jumbo text-white'>
+        <h1 className='text-jumbo tracking-in-expand-fwd'>
+          Impulsione o potencial do seu negócio nossas soluções TI de última geração.
+        </h1>
+        <p>
+          Nós fornecemos soluções personalizadas que ajudam sua startup a crescer e
+          se destacar no mercado digital.
+        </p>
+      </div>
+      <hr />
     </div>
   </S.JumbotronConfig>
 );
