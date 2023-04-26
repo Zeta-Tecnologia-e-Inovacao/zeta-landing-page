ae89fb8f602acfcb6d9446794bffe24751bfdd2f
Pedro Henrique Mota <pedrohmota2005@gmail.com>
2023-04-26 17:56:52 -0300

    fix: ESLint funcionando

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,8 +3,8 @@ import type { AppProps } from 'next/app';
 import '../styles/global.css';
 import 'bootstrap/dist/css/bootstrap.css';
 
-const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
-};
+const App = ({ Component, pageProps }: AppProps) => (
+  <Component {...pageProps} />
+);
 
 export default App;
