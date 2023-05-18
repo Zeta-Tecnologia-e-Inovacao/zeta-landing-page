c0daccadedbbdbf9d95471f6956e881726c98e2e
PedrooMota <pedrohmota2005@gmail.com>
2023-05-18 15:15:54 -0300

    fix: teste

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 // next.config.js
-const isProd = process.env.NODE_ENV === 'production'
 
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: isProd ? '/zeta-landing-page/' : '',
   images: {
     unoptimized: true,
   },
