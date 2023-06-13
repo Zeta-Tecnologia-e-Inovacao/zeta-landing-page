6c7bdda5794d763b442333604f209d23351aebad
PedrooMota <pedrohmota2005@gmail.com>
2023-06-13 18:09:45 -0300

    feat: add new Header/Jumbotron and Global CSS

--- src/components/header/Navbar.tsx
+++ src/components/header/Navbar.tsx
@@ -1,51 +1,68 @@
 import React from 'react';
 import type { NextPage } from 'next';
+import Link from 'next/link';
 import { Image } from '@nextui-org/react';
+import Script from 'next/script';
 import * as S from './styles';
 
 const Header: NextPage = () => (
 
   <S.MenuHeader>
-    <div className='container-fluid '>
-      <header className='row' id='header'>
-        <div className='text-transition-slider-logo'>
-          <div id='logo-container'>
-            <Image src='images/Zeta-Logo.png' alt='' id='logo' />
-          </div>
-          <h4><i>Tecnologia e Inovação</i></h4>
-        </div>
-      </header>
-      <nav className='navbar navbar-expand-lg text-transition-slider-navbar'>
-        <button className='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#navbarNavDropdown' aria-controls='navbarNavDropdown' aria-expanded='false' aria-label='Toggle navigation'>
-          <i className='navbar-toggler-icon' />
-        </button>
-        <div className='collapse navbar-collapse' id='navbarNavDropdown'>
-          <ul className='navbar-nav mx-auto'>
-            <li className='nav-item'>
-              <a className='nav-link active' aria-current='page' href='#home'>Home</a>
-            </li>
+    <Script
+      src='https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js'
+      integrity='sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r'
+      crossOrigin='anonymous'
+    />
+    <Script
+      src='https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js'
+      integrity='sha384-fbbOQedDUMZZ5KreZpsbe1LCZPVmfTnH7ois6mU1QK+m14rQ1l2bGBq41eYeM/fS'
+      crossOrigin='anonymous'
+    />
 
+    {/* menu-absolute  navbar fixed-top */}
+    <nav className='navbar navbar-expand-lg bg-nav fixed-top' id='header'>
+      <div className='container'>
+        <a className='navbar-brand' href='##'>
+          <Image src='/images/Zeta-Logo.png' className='logo' alt='logo' width={200} height={200} />
+        </a>
+        <button aria-controls='navbarSupportedContent' aria-expanded='false' aria-label='Toggle navigation' className='navbar-toggler' data-bs-target='#navbarSupportedContent' data-bs-toggle='collapse' type='button'><span className='navbar-toggler-icon' /></button>
+        <div className='collapse navbar-collapse' id='navbarSupportedContent'>
+          <ul className='navbar-nav ms-auto mb-2 mb-lg-0'>
             <li className='nav-item'>
-              <a className='nav-link' href='#soluction'>Soluções</a>
+              <Link className='nav-link active' href='/'>Home</Link>
             </li>
-
             <li className='nav-item'>
-              <a className='nav-link' href='#parcerias'>Parcerias</a>
+              <a className='nav-link' href='#services'>Serviços</a>
             </li>
-
-            <li className='nav-item'>
-              <a className='nav-link' href='#contact'>Contato</a>
-            </li>
-
             <li className='nav-item'>
               <a className='nav-link' href='#about'>Sobre nós</a>
             </li>
-
+            <li className='nav-item'>
+              <a className='nav-link' href='#parcerias'>Parcerias</a>
+            </li>
+            <li className='nav-item'>
+              <a className='nav-link' href='#contact'>Contato</a>
+            </li>
           </ul>
         </div>
-      </nav>
-    </div>
-
+      </div>
+    </nav>
+    <section className='slider-area bgs-cover pt-185 pb-160 sliderT animated fadeInUp'>
+      <div className='container'>
+        <div className='main-slider-active'>
+          <div className='slider-item'>
+            <div className='slide-content text-white'>
+              <span className='sub-title'>Aproveite o poder da tecnologia! </span>
+              <span className='h2'>Explore o </span>
+              <h1>futuro!</h1>
+              <a href='#services' className='theme-btn2'>
+                Vamos começar
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   </S.MenuHeader>
 
 );
