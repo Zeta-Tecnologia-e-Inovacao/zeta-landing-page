d2da0cf6761bb8bf9ed28df4aff17ebe219f125c
PedrooMota <pedrohmota2005@gmail.com>
2023-06-14 14:57:11 -0300

    feat: add new components in footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,68 +1,141 @@
 import { NextPage } from 'next';
+import Link from 'next/link';
 import Image from 'next/image';
 import * as S from './styles';
 
 const Footer: NextPage = () => (
 
   <S.Footer>
-    <section id='footer'>
-      <footer className='container-fluid bg-blue-color'>
+    <footer className='main-footer bgc-gray footer-white rel z-1'>
+      <div className='footer-cta-comp'>
         <div className='container'>
-          <div className='row'>
-            {/* <!-- FOOTER TOP --> */}
-            <div className='col-12' id='footer-top'>
-              <div className='row justify-content-between'>
-                <div className='col-4'>
-                  <Image src='/images/Zeta-Logo.png' alt='logo' className='img-fluid' width={150} height={100} />
-                </div>
-              </div>
+          <div className='footer-banner-inner bgs-cover image'>
+            <div className='section-title wow fadeInLeft delay-0-2s'>
+              <span className='title-footer'>O que você está esperando?</span>
+              <h3 className='sub-title-footer'>Entre em contato conosco!</h3>
             </div>
-            {/* <!-- FOOTER DETAILS --> */}
-            <div className='col-12' id='footer-details'>
-              <div className='row'>
-                <div className='col-12 col-md-4' id='news-container'>
-                  <h4>Fique por dentro das novidades</h4>
-                  <p className='secondary-color'>
-                    Estamos comprometidos em oferecer soluções que possam fazer a
-                    diferença na sua vida pessoal ou profissional. Nossos produtos/serviços
-                    desenvolvidos com qualidade e tecnologia de ponta, para que você possa
-                    do melhor que o mercado tem a oferecer.
-                  </p>
-                </div>
-                <div className='col-12 col-md-4' id='contact-container'>
-                  <h4>Nossa Localização</h4>
-                  {/* ====== UTILIZEI MEIOS DE CONTATO FICTICIOS ==== */}
-                  <p className='secondary-color'>Brasil - São Paulo</p>
-                  <h4>Nosso Contato</h4>
-                  <p className='secondary-color'>+55(12) 99606-2530</p>
-                  <p className='secondary-color'>zeta.technologia@gmail.com</p>
-                </div>
-                <div className='col-12 col-md-4' id='links-container'>
-                  <div className='row'>
-                    <h4>Você pode buscar por:</h4>
-                    <div className='col-6'>
-                      <ul className='list-unstyled'>
-                        <li><a href='#h' className='secondary-color'>Home</a></li>
-                        <li><a href='#soluctions' className='secondary-color'>Soluções</a></li>
-                        <li><a href='#parcerias' className='secondary-color'>Parcerias</a></li>
-                        <li><a href='#contact' className='secondary-color'>Contato</a></li>
-                        <li><a href='#about' className='secondary-color'>Sobre nós</a></li>
-                      </ul>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            {/* <!-- FOOTER BOTTOM --> */}
-            <div className='col-12' id='footer-bottom'>
-              <div className='row justify-content-center text-center'>
-                <p className='secondary-color'>&copy;2023 Zeta | Tecnologia & Inovação. All righs reserved.</p>
+            <a href='#contact' className='button-footer'>
+              Saiba mais
+            </a>
+            <div className='hotline'>
+              <div className='content'>
+                <i className='bi bi-check2-circle'><a className='text-phone-two' href='callto:+55(12)99606-2530'>+55(12) 99606-2530</a></i>
               </div>
             </div>
           </div>
         </div>
-      </footer>
-    </section>
+      </div>
+      <div className='container'>
+        <div className='row medium-gap '>
+          <div className='col-xl-3 col-sm-6'>
+            <div className='footer-widget widget_about'>
+              <div className='footer-logo mb-30'>
+                <a href='index'><Image src='/images/Zeta-Logo.png' alt='Logo' width={140} height={40} /></a>
+              </div>
+              <p>
+                Estamos comprometidos em oferecer soluções que possam fazer a diferença na sua
+                vida pessoal ou profissional.
+              </p>
+            </div>
+          </div>
+          <div className='col-xl-3 col-sm-6 order-xl-2'>
+            <div className='footer-widget widget_newsletter'>
+              <h4 className='footer-title'>Nossa localização</h4>
+              <p>
+                <i className='bi bi-geo-alt-fill' />
+                Brazil - São Paulo
+              </p>
+              <p>
+                <i className='bi bi-geo-alt-fill' />
+                Parque Tecnológico - Nexus
+              </p>
+              <h5>Redes Sociais</h5>
+              <div className='social-style-one'>
+                <a href='https://www.facebook.com/'>
+                  <i className='bi bi-facebook' />
+                </a>
+                <a href='https://www.instagram.com/'>
+                  <i className='bi bi-instagram' />
+                </a>
+                <a href='https://www.linkedin.com/'>
+                  <i className='bi bi-linkedin' />
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className='col-xl-6'>
+            <div className='row'>
+              <div className='col-md-6 col-6 col-small'>
+                <div className='footer-widget widget_nav_menu'>
+                  <h4 className='footer-title'>Links</h4>
+                  <ul className='list-style-two'>
+                    <li>
+                      <i className='bi bi-arrow-right-circle' />
+                      <Link href='/'> Home</Link>
+                    </li>
+                    <li>
+                      <i className='bi bi-arrow-right-circle' />
+                      <a href='#about'> Sobre nós</a>
+                    </li>
+
+                    <li>
+                      <i className='bi bi-arrow-right-circle' />
+                      <a href='#services'> Serviços</a>
+                    </li>
+                    <li>
+                      <i className='bi bi-arrow-right-circle' />
+                      <a href='#parceiros'> Parcerias</a>
+                    </li>
+                    <li>
+                      <i className='bi bi-arrow-right-circle' />
+                      <a href='#contact'> Nosso contato</a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div className='col-md-6 col-6 col-small'>
+                <div className='footer-widget widget_nav_menu wow fadeInUp delay-0-4s'>
+                  <h4 className='footer-title'>Serviços</h4>
+                  <ul className='list-style-two'>
+
+                    <li>
+                      <i className='bi bi-arrow-right-circle' />
+                      <a href='#services'> Relé Bimanual</a>
+                    </li>
+                    <li>
+                      <i className='bi bi-arrow-right-circle' />
+                      <a href='#services'> Desenvolvimento Web</a>
+                    </li>
+                    <li>
+                      <i className='bi bi-arrow-right-circle' />
+                      <a href='#services'> Sistemas embarcados e IOT</a>
+                    </li>
+                    <li>
+                      <i className='bi bi-arrow-right-circle' />
+                      <a href='#services'> Cloud Computing</a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='footer-bottom bgc-black mt-20 pt-20'>
+        <div className='container'>
+          <div className='row align-items-center'>
+            <div className='col-lg-12'>
+              <div className='copyright-text text-lg-center'>
+                <p>©2023 Zeta | Tecnologia e Informação.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='footer-shapes'>
+        <Image className='shape two' src='/images/footer/footer-bg-shape.png' alt='Shape' width={530} height={433} />
+      </div>
+    </footer>
   </S.Footer>
 
 );
--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -1,109 +1,272 @@
 import styled from 'styled-components';
 
 export const Footer = styled.div`
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&family=Roboto:wght@400;500;700&display=swap');
 
 
-/* FOOTER TOP */
-/* #footer {
-  padding: 3em 2em;
-} */
-#footer-top {
-  margin-bottom: 2em;
+/******************* ## Banner-Footer *******************/
+/*******************************************************/
+@media only screen and (min-width: 1200px) {
+  .footer-cta-comp .container {
+    max-width: 850px; 
+  } 
 }
-#footer-top,
-#footer-top i {
-  color: #fff;
+
+.image{
+  background-image: url(/images/footer/footer-bg.png);
 }
-#social-icons {
-  text-align: right;
+
+.footer-banner-inner .title-footer{
+  font-size: 20px;
+  color: white;
 }
-#social-icons i {
-  margin-right: 1em;
-  font-size: 1.5em;
-  cursor: pointer;
-}
-#social-icons i:hover {
-  color: #fff;
-}
-/* FOOTER DETAILS */
-#footer-details {
-  margin-bottom: 3em;
-}
-#news-container,
-#contact-container,
-#links-container {
-  padding: 0;
-  border: 0;
-  margin-bottom: 2em;
-}
-#footer-details h4 {
-  color: #fff;
-  margin-bottom: 1.5em;
-}
-#news-container input {
-  border-radius: 0;
-  border: none;
-}
-#links-container li {
-  margin-bottom: 0.5em;
-}
-#links-container a {
+
+.footer-banner-inner .sub-title-footer{
+  color: black;
+  transition: 0.5s;
   text-decoration: none;
+  font-size: 28px;
 }
-#links-container a:hover {
+
+.footer-banner-inner .text-phone{
+  text-decoration: none;
+  font-size: 18px;
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+}
+
+.footer-banner-inner .text-phone-two{
+  color: black;
+  text-decoration: none;
+  font-size: 16px;
+  font-family: 'Poppins', sans-serif;
+  font-weight: 400;
+}
+
+.footer-banner-inner {
+  display: flex;
+  -ms-flex-wrap: wrap;
+  flex-wrap: wrap;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  align-items: center;
+  padding: 60px 80px 35px;
+  -webkit-transform: translateY(-80px);
+  -ms-transform: translateY(-80px);
+  transform: translateY(-80px);
+  z-index: 1;
+  position: relative; 
+}
+
+.footer-banner-inner::before {
+  position: absolute;
+  content: "";
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+  top: 0;
+  left: 0;
+  opacity: 0.9;
+  background-color: var(--secondary-color); 
+}
+
+@media only screen and (max-width: 767px) {
+  .footer-banner-inner {
+    padding-left: 35px;
+    padding-right: 35px; 
+  } 
+}
+
+@media only screen and (max-width: 375px) {
+  .footer-banner-inner {
+    padding-left: 25px;
+    padding-right: 25px; 
+  } 
+}
+
+.footer-banner-inner .section-title {
+  margin-bottom: 25px;
+  margin-right: auto; 
+}
+
+.footer-banner-inner .section-title .sub-title {
+  margin-bottom: 8px;
+  color: var(--heading-color); 
+}
+
+      @media only screen and (max-width: 375px) {
+        .footer-banner-inner .section-title .sub-title {
+          font-size: 18px; 
+        } 
+      }
+
+    .footer-banner-inner .section-title h2 {
+      margin-bottom: 0;
+      letter-spacing: -1px; 
+    }
+
+  .footer-banner-inner .theme-btn {
+    margin-bottom: 25px;
+    margin-right: 30px; 
+  }
+
+.footer-banner-inner .hotline {
+  margin-bottom: 25px; 
+}
+.footer-banner-inner .hotline > i {
+  background: black;
+  border-color: white; 
+}
+.footer-banner-inner .hotline .content span, .footer-banner-inner .hotline .content a {
+  color: var(--heading-color); 
+}
+
+.footer-logo {
+  padding: 25px 30px;
+  display: inline-block;
+  -webkit-clip-path: polygon(6% 0%, 100% 0%, 94% 100%, 0% 100%);
+  clip-path: polygon(6% 0%, 100% 0%, 94% 100%, 0% 100%); 
+}
+
+.footer-white {
+  color: #b6bace; 
+}
+
+.footer-white a:not(:hover) {
+  color: #b6bace; 
+}
+
+.footer-white .read-more {
+  color: var(--primary-color); 
+}
+
+.footer-white h5,
+.footer-white .footer-title{
+  color: white; 
+}
+
+.footer-white .widget_newsletter form {
+  border-color: rgba(255, 255, 255, 0.3); 
+}
+
+.footer-widget {
+  margin-bottom: 50px; 
+}
+
+.footer-title {
+  margin-bottom: 28px; 
+}
+
+@media only screen and (min-width: 376px) {
+  .footer-title {
+    font-size: 24px; 
+  } 
+}
+
+.widget_newsletter .social-style-one a {
+  margin-right: 15px; }
+
+/* Footer Bottom */
+.footer-bottom-menu ul {
+  -ms-flex-wrap: wrap;
+  flex-wrap: wrap;
+  display: inline-flex;
+  margin-right: -10px; 
+}
+
+.footer-bottom-menu ul li {
+  margin: 0 10px 5px 0; 
+}
+
+.footer-bottom-menu ul li a {
+  color: var(--heading-color); 
+}
+
+.footer-bottom-menu ul li a:hover {
+  text-decoration: underline; }
+  .footer-bottom-menu ul li:not(:last-child):after {
+    content: '|';
+    margin-left: 10px; }
+
+.footer-shapes .shape {
+  position: absolute;
+  z-index: -2;
+  max-width: 50%; }
+  .footer-shapes .shape.one {
+    left: 0;
+    bottom: 0; }
+  .footer-shapes .shape.two {
+    right: 0;
+    bottom: 0; }
+  .footer-shapes .shape.three {
+    right: 0;
+    bottom: 10%;
+    z-index: -1; }
+
+    .show_app_slider .slick-dots{
+      margin-top: 5px;
+      justify-content: center;
+    }
+
+.bi-check2-circle{
+  color: white;
+  font-size: 18px;
+}
+
+    /* Botão */
+.button-footer {
+  background-color: black;
+  border: 0;
+  border-radius: 20px;
   color: #fff;
+  cursor: pointer;
+  display: inline-block;
+  font-family: system-ui,-apple-system,system-ui,"Segoe UI",Roboto,Ubuntu,"Helvetica Neue",sans-serif;
+  font-size: 18px;
+  font-weight: 600;
+  outline: 0;
+  padding: 16px 21px;
+  position: relative;
+  text-align: center;
+  text-decoration: none;
+  transition: all .3s;
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
 }
-/* FOOTER BOTTOM */
-#footer-bottom {
-  border-top: 1px solid #7a7a7a;
-  padding-top: 2em;
+
+.button-footer:before {
+  background-color: initial;
+  background-image: linear-gradient(black 0, rgba(255, 255, 255, 0) 100%);
+  border-radius: 125px;
+  content: "";
+  height: 50%;
+  left: 4%;
+  opacity: .5;
+  position: absolute;
+  top: 0;
+  transition: all .3s;
+  width: 92%;
 }
-#footer-bottom i {
-  color: #b6bace  ;
+
+.button-footer:hover {
+  box-shadow: rgba(255, 255, 255, .2) 0 3px 15px inset, rgba(0, 0, 0, .1) 0 3px 5px, rgba(0, 0, 0, .1) 0 10px 13px;
+  transform: scale(1.05);
 }
 
 @media (min-width: 768px) {
-  #news-container {
-    padding-right: 2em;
-  }
-  #contact-container {
-    padding: 0 2em;
-    border-left: 1px solid #7a7a7a;
-    border-right: 1px solid #7a7a7a;
-  }
-  #links-container {
-    padding-left: 2em;
+  .button-footer {
+    padding: 16px 48px;
   }
 }
 
-button {
-  padding: 17px 40px;
-  border-radius: 50px;
-  border: 0;
-  background-color: white;
-  color: black;
-  box-shadow: rgb(0 0 0 / 5%) 0 0 8px;
-  letter-spacing: 1.5px;
-  text-transform: uppercase;
-  font-size: 15px;
-  transition: all .5s ease;
+a{
+  text-decoration: none;
 }
- 
-button:hover {
-  letter-spacing: 3px;
-  background-color: hsl(0deg 0% 5%);
-  color: hsl(0, 0%, 100%);
-  box-shadow: rgb(4 20 50) 0px 7px 15px 0px;
+
+.list-style-two i{
+  color: white;
 }
- 
-button:active {
-  letter-spacing: 3px;
-  background-color: hsl(261deg 80% 48%);
-  color: hsl(0, 0%, 100%);
-  box-shadow: rgb(93 24 220) 0px 0px 0px 0px;
-  transform: translateY(10px);
-  transition: 100ms;
-}
+
 `;
 
 export default Footer;
