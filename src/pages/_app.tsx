9914bc2e4d3b454b43015b11498ed09e25fa7cdc
PedrooMota <pedrohmota2005@gmail.com>
2023-08-28 14:57:54 -0300

    fix: react toastify corrigido

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import '@/styles/global.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'react-toastify/dist/ReactToastify.css';
 import type { AppProps } from 'next/app';
 
 const App = ({ Component, pageProps }: AppProps) => (
